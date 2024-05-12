9a6be164d836527edec7f9e82542804927675868
Marcio Sbicigo <marciosbicigo@hotmail.com>
2024-05-12 17:44:45 -0300

    Update router_login.py

--- app/routers/router_login.py
+++ app/routers/router_login.py
@@ -18,17 +18,25 @@ def create_token(data: dict, secret_key=Secrets.SECRET_KEY):
 
 @router.post("/api/login")
 async def login(request: LoginRequest):
-    try:
-        user = get_users_collection().find_one({"username": request.username})
+    
+    user = get_users_collection().find_one({"username": request.username})
         
-        # Verificando se o usuário existe no banco de dados ou se a senha está incorreta
-        if not user or not verify_password(request.password, user['password']):
-            raise HTTPException(status_code=401, detail="Invalid username or password.")
-        
-        else:
-                               
+    # Verificando se o usuário existe no banco de dados ou se a senha está incorreta
+    if not user or not verify_password(request.password, user['password']):
+        raise HTTPException(status_code=401, detail="Invalid username or password.")
+    
+    else:
+        try:
+            
+            # user = get_users_collection().find_one({"username": request.username})
+            
+            # # Verificando se o usuário existe no banco de dados ou se a senha está incorreta
+            # if not user or not verify_password(request.password, user['password']):
+            #     raise HTTPException(status_code=401, detail="Invalid username or password.")
+
+                                
             session_id = str(uuid.uuid4())
-                
+                    
             current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
             expire_time = datetime.now() + timedelta(hours=2)
@@ -40,13 +48,13 @@ async def login(request: LoginRequest):
                 'login_time': current_time,
                 'expire_time': expire_time
             }
-                
+                    
             # Atualiza o session_id e a data do último login no objeto do do usuário
             get_users_collection().update_one({"_id": ObjectId(user["_id"])}, {"$set": {"last_login": current_time}})
-                
+                    
             # Insere uma nova sessão
             get_sessions_collection().insert_one(new_session)
-                
+                    
             # Gera os dados contidos no token de retorno
             user_data = {
                 "username": user["username"],
@@ -54,15 +62,15 @@ async def login(request: LoginRequest):
                 "full_name": user["full_name"],
                 "session_id": session_id
             }
-                
+                    
             # Cria o token jwt
             token = create_token(user_data)
-                
+                    
             # Retorna o token
             return JSONResponse(content={"Status": "Successful login", "access_token": token})
-    except Exception as error:
-        print(f'Authentication route error: {error}')
-        raise HTTPException(status_code=500, detail="Internal error from authentication route.")
+        except Exception as error:
+            print(f'Authentication route error: {error}')
+            raise HTTPException(status_code=500, detail="Internal error from authentication route.")
     
 # Rota que verifica se a sessão do usuário é válida
 @router.get('/api/session')
